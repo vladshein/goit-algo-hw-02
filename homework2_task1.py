713decda47c45d5e765743959cebc8d2be4c4963
vladshein <vlshein1@gmail.com>
2024-09-30 18:55:08 +0300

    Updated both solutions

--- homework2_task1.py
+++ homework2_task1.py
@@ -14,7 +14,6 @@ def generate_request(queue: Queue):
     for i in range(requests_count):
         queue.put(i)
         print(f"New member added to queue")
-    print(queue)
 # Функція process_request():
 #     Якщо черга не пуста:
 #         Видалити заявку з черги
@@ -23,13 +22,12 @@ def generate_request(queue: Queue):
 #         Вивести повідомлення, що черга пуста
 
 def process_requests(queue: Queue):
-    print(queue)
     if queue.empty():
         print("Queue is empty")
     else:
         while not queue.empty():
             number = queue.get()
-            print(f"Request from queue is {number}")
+            print(f"Process qequest from queue is {number}")
 
 # Головний цикл програми:
 #     Поки користувач не вийде з програми:
@@ -38,7 +36,7 @@ def process_requests(queue: Queue):
 
 while(True):
     generate_request(queue)
-    print(queue)
+    # print(queue)
     process_requests(queue)
     value = input("Do you want to stop processing? Enter y to stop, n to add new requests\n")
     if value == 'y':
